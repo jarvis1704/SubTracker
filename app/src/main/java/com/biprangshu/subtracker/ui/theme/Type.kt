fe762a8e3c6c11abc51f9a61de454ac9aac9ebd2
jarvis1704 <dasbiprangshu@gmail.com>
2025-12-23 19:21:07 +0530

    added robotoflex font

--- app/src/main/java/com/biprangshu/subtracker/ui/theme/Type.kt
+++ app/src/main/java/com/biprangshu/subtracker/ui/theme/Type.kt
@@ -211,4 +211,49 @@ object AppFonts {
             )
         )
     )
+
+    //roboto flex
+    @OptIn(ExperimentalTextApi::class)
+    val robotoFlexTopBar = FontFamily(
+        Font(
+            R.font.robotoflex_variable,
+            variationSettings = FontVariation.Settings(
+                FontVariation.width(125f),
+                FontVariation.weight(1000),
+                FontVariation.grade(0),
+                FontVariation.Setting("XOPQ", 96F),
+                FontVariation.Setting("XTRA", 500F),
+                FontVariation.Setting("YOPQ", 79F),
+                FontVariation.Setting("YTAS", 750F),
+                FontVariation.Setting("YTDE", -203F),
+                FontVariation.Setting("YTFI", 738F),
+                FontVariation.Setting("YTLC", 514F),
+                FontVariation.Setting("YTUC", 712F)
+            )
+        )
+    )
+
+    @OptIn(ExperimentalTextApi::class)
+    val robotoFlexHeadline = FontFamily(
+        Font(
+            R.font.robotoflex_variable,
+            variationSettings = FontVariation.Settings(
+                FontVariation.width(130f),
+                FontVariation.weight(600),
+                FontVariation.grade(0)
+            )
+        )
+    )
+
+    @OptIn(ExperimentalTextApi::class)
+    val robotoFlexTitle = FontFamily(
+        Font(
+            R.font.robotoflex_variable,
+            variationSettings = FontVariation.Settings(
+                FontVariation.width(130f),
+                FontVariation.weight(700),
+                FontVariation.grade(0)
+            )
+        )
+    )
 }
