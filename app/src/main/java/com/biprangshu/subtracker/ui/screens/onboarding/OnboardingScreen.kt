d98dc29c78ba4bd2781e877851bb56a982720488
jarvis1704 <dasbiprangshu@gmail.com>
2026-01-02 11:01:02 +0530

    added onboardingscreen files

---
+++ app/src/main/java/com/biprangshu/subtracker/ui/screens/onboarding/OnboardingScreen.kt
@@ -0,0 +1,11 @@
+package com.biprangshu.subtracker.ui.screens.onboarding
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun OnBoardingScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
