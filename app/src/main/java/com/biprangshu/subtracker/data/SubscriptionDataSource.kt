068e05b224c0793a4ec87bea7a907e120a0692a1
jarvis1704 <dasbiprangshu@gmail.com>
2026-01-19 12:19:14 +0530

    added colour to the subscription list

--- app/src/main/java/com/biprangshu/subtracker/data/SubscriptionDataSource.kt
+++ app/src/main/java/com/biprangshu/subtracker/data/SubscriptionDataSource.kt
@@ -2,10 +2,10 @@ package com.biprangshu.subtracker.data
 
 import com.biprangshu.subtracker.R
 
-
 data class SubscriptionService(
     val name: String,
-    val iconRes: Int
+    val iconRes: Int,
+    val color: Long // Stores ARGB Color as Long (e.g. 0xFFE50914)
 )
 
 /**
@@ -13,41 +13,41 @@ data class SubscriptionService(
  */
 object SubscriptionDataSource {
     val popularServices = listOf(
-        SubscriptionService("Netflix", R.drawable.netflix_logo),
-        SubscriptionService("Spotify", R.drawable.spotify_logo),
-        SubscriptionService("Disney+", R.drawable.disney),
-        SubscriptionService("YouTube Premium", R.drawable.youtube),
-        SubscriptionService("Discord Nitro", iconRes = R.drawable.discord_logo),
-        SubscriptionService("Hulu", R.drawable.hulu_logo),
-        SubscriptionService("HBO Max", R.drawable.hbo_max),
-        SubscriptionService("Google One", R.drawable.google_one),
-        SubscriptionService("Microsoft 365", R.drawable.microsoft_365),
-        SubscriptionService("Dropbox", R.drawable.dropbox),
-        SubscriptionService("Slack", R.drawable.slack),
-        SubscriptionService("Tidal", R.drawable.tidal),
-        SubscriptionService("Audible", R.drawable.audible),
-        SubscriptionService("Canva", R.drawable.canva),
-        SubscriptionService("Evernote", R.drawable.evernote),
-        SubscriptionService("LinkedIn Premium", R.drawable.linkedin_logo),
-        SubscriptionService("Zoom", R.drawable.zoom),
-        SubscriptionService("Adobe Creative Cloud", R.drawable.adobe_logo),
-        SubscriptionService("NordVPN", R.drawable.nordvpn),
-        SubscriptionService("LastPass", R.drawable.lastpass),
-        SubscriptionService("ProtonVPN", R.drawable.protonvpn),
-        SubscriptionService("Skillshare", R.drawable.skill_share),
-        SubscriptionService("Coursera", R.drawable.coursera),
-        SubscriptionService("Udemy", R.drawable.udemy),
-        SubscriptionService("Twitch Turbo", R.drawable.twitch),
-        SubscriptionService("Fitbit Premium", R.drawable.fitbit),
-        SubscriptionService("Headspace", R.drawable.headspace),
-        SubscriptionService("Calm", R.drawable.calm),
-        SubscriptionService("Strava", R.drawable.strava),
-        SubscriptionService("Peloton", R.drawable.peloton),
-        SubscriptionService("Scribd", R.drawable.scribd),
-        SubscriptionService("Blinkist", R.drawable.blinkist),
-        SubscriptionService("Duolingo Plus", R.drawable.duolingo),
-        SubscriptionService("MyFitnessPal Premium", R.drawable.nyfitnesspal),
-        SubscriptionService("Amazon Prime", R.drawable.amazon_prime),
-        SubscriptionService("Apple Music", R.drawable.apple_music)
+        SubscriptionService("Netflix", R.drawable.netflix_logo, 0xFFE50914),
+        SubscriptionService("Spotify", R.drawable.spotify_logo, 0xFF1DB954),
+        SubscriptionService("Disney+", R.drawable.disney, 0xFF113CCF),
+        SubscriptionService("YouTube Premium", R.drawable.youtube, 0xFFFF0000),
+        SubscriptionService("Discord Nitro", R.drawable.discord_logo, 0xFF5865F2),
+        SubscriptionService("Hulu", R.drawable.hulu_logo, 0xFF1CE783),
+        SubscriptionService("HBO Max", R.drawable.hbo_max, 0xFF9E86FF),
+        SubscriptionService("Google One", R.drawable.google_one, 0xFF4285F4),
+        SubscriptionService("Microsoft 365", R.drawable.microsoft_365, 0xFFF25022),
+        SubscriptionService("Dropbox", R.drawable.dropbox, 0xFF0061FE),
+        SubscriptionService("Slack", R.drawable.slack, 0xFF4A154B),
+        SubscriptionService("Tidal", R.drawable.tidal, 0xFF000000),
+        SubscriptionService("Audible", R.drawable.audible, 0xFFF79C1F),
+        SubscriptionService("Canva", R.drawable.canva, 0xFF00C4CC),
+        SubscriptionService("Evernote", R.drawable.evernote, 0xFF00A82D),
+        SubscriptionService("LinkedIn Premium", R.drawable.linkedin_logo, 0xFF0077B5),
+        SubscriptionService("Zoom", R.drawable.zoom, 0xFF2D8CFF),
+        SubscriptionService("Adobe Creative Cloud", R.drawable.adobe_logo, 0xFFFF0000),
+        SubscriptionService("NordVPN", R.drawable.nordvpn, 0xFF4687FF),
+        SubscriptionService("LastPass", R.drawable.lastpass, 0xFFD32D27),
+        SubscriptionService("ProtonVPN", R.drawable.protonvpn, 0xFF6D4AFF),
+        SubscriptionService("Skillshare", R.drawable.skill_share, 0xFF00FF84),
+        SubscriptionService("Coursera", R.drawable.coursera, 0xFF0056D2),
+        SubscriptionService("Udemy", R.drawable.udemy, 0xFFA435F0),
+        SubscriptionService("Twitch Turbo", R.drawable.twitch, 0xFF9146FF),
+        SubscriptionService("Fitbit Premium", R.drawable.fitbit, 0xFF00B0B9),
+        SubscriptionService("Headspace", R.drawable.headspace, 0xFFF47D31),
+        SubscriptionService("Calm", R.drawable.calm, 0xFF00A4FF),
+        SubscriptionService("Strava", R.drawable.strava, 0xFFFC5200),
+        SubscriptionService("Peloton", R.drawable.peloton, 0xFFDF1C2F),
+        SubscriptionService("Scribd", R.drawable.scribd, 0xFF1A7B88),
+        SubscriptionService("Blinkist", R.drawable.blinkist, 0xFF2CE080),
+        SubscriptionService("Duolingo Plus", R.drawable.duolingo, 0xFF58CC02),
+        SubscriptionService("MyFitnessPal Premium", R.drawable.nyfitnesspal, 0xFF0066EE),
+        SubscriptionService("Amazon Prime", R.drawable.amazon_prime, 0xFF00A8E1),
+        SubscriptionService("Apple Music", R.drawable.apple_music, 0xFFFC3C44)
     )
 }
