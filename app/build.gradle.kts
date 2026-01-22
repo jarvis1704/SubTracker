cd8f9d3b94e777b173f7156bc3926e4d238994a2
jarvis1704 <dasbiprangshu@gmail.com>
2026-01-22 11:30:15 +0530

    gradle: version bump for play release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.biprangshu.subtracker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.6 (play)"
+        versionCode = 8
+        versionName = "1.7 (play)"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
